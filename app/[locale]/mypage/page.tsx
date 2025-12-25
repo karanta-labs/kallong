eec4c46408b101a17ba2fd042cb9c382de9e5393
rladus <knyeon26@gmail.com>
2025-12-25 11:53:45 +0900

    feat: 코드 리펙토링, 스타일 수정

--- app/[locale]/mypage/page.tsx
+++ app/[locale]/mypage/page.tsx
@@ -1,10 +1,8 @@
 import { LookbookList } from '@/components/mypage/lookbook-list';
-import { Profile } from '@/components/mypage/profile';
 
 export default function MyPage() {
   return (
     <div className='relative bg-white flex flex-1 flex-col gap-10'>
-      <Profile />
       <LookbookList />
     </div>
   );
