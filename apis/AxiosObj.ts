0d9a7121599c2665a64531f56e771a40c6224c8f
rladus <knyeon26@gmail.com>
2026-06-02 08:47:47 +0900

    feat: utils 함수 정리

--- apis/AxiosObj.ts
+++ apis/AxiosObj.ts
@@ -2,6 +2,14 @@ import axios, { type AxiosInstance } from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
+// API 응답 데이터 타입 (모든 API 응답의 기본 구조)
+export interface ApiResponse<T = unknown> {
+  data: T;
+  message: string;
+  result: string;
+  resultCode: number;
+}
+
 const createAxiosInstance = (): AxiosInstance => {
   const axiosObj = axios.create({
     baseURL: baseURL,
